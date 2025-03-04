a24798e0abbd4e6a06212a3bb51cee5cb99a7e1c
creme332 <65414576+creme332@users.noreply.github.com>
2025-03-04 17:01:46 +0400

    implement `unmerge` and `split` function

--- src/validator.cpp
+++ src/validator.cpp
@@ -13,3 +13,77 @@ std::pair<bool, std::string> Validator::validate(const std::string &str1,
   }
   return {true, "Validation successful."};
 }
+
+std::deque<std::string> Validator::unmerge(std::string row) {
+  // put each column in an array
+  std::vector columns = split(row, ',');
+
+  // create an accumulator to store result
+  std::deque<std::string> acc;
+
+  unmerge(columns, columns.size() - 1, acc);
+
+  return acc;
+}
+
+void Validator::unmerge(std::vector<std::string> columns, int i,
+                        std::deque<std::string> &acc) {
+  if (i < 0 || columns.size() == 0)
+    return;
+
+  // check if "|" symbol is not present in columns[i]
+  if (columns[i].find('|') == std::string::npos) {
+    if (acc.size() == 0) {
+      acc.push_back(columns[i]);
+    } else {
+      // prepend columns[i] to each element in accumulator
+      for (int j = 0; j < acc.size(); j++) {
+        acc[j] = columns[i] + "," + acc[j];
+      }
+    }
+    unmerge(columns, i - 1, acc);
+    return;
+  }
+
+  // "|" symbol is present in columns
+  // get each possible value for current column
+  std::vector groups = split(columns[i], '|');
+
+  // initialize acc if empty
+  if (acc.size() == 0) {
+    // push all elements of groups into acc
+    for (auto g : groups) {
+      acc.push_back(g);
+    }
+    unmerge(columns, i - 1, acc);
+    return;
+  }
+
+  // create all possible combinations by combining each element of groups
+  // with each element of acc
+  int count = acc.size();
+  while (count--) {
+    // pop first element of acc
+    std::string front = acc.front();
+    acc.pop_front();
+
+    // add new combination to acc
+    for (std::string g : groups) {
+      acc.push_back(g + "," + front);
+    }
+  }
+
+  unmerge(columns, i - 1, acc);
+}
+
+std::vector<std::string> Validator::split(const std::string &s, char delim) {
+  std::vector<std::string> result;
+  std::stringstream ss(s);
+  std::string item;
+
+  while (getline(ss, item, delim)) {
+    result.push_back(item);
+  }
+
+  return result;
+}
--- src/validator.h
+++ src/validator.h
@@ -1,12 +1,61 @@
 #ifndef VALIDATOR_H
 #define VALIDATOR_H
 
+#include <deque>
+#include <iostream>
+#include <sstream>
 #include <string>
 #include <utility> // for std::pair
+#include <vector>
 
+/**
+ * @class Validator
+ * @brief Contains functions for validating the output of a heuristic algorithm.
+ *
+ */
 class Validator {
 public:
-    std::pair<bool, std::string> validate(const std::string& str1, const std::string& str2);
+  std::pair<bool, std::string> validate(const std::string &str1,
+                                        const std::string &str2);
+
+  /**
+   * @brief Unmerges a row and returns the initial list of rows.
+   * For example `unmerge('1|2|3,2')` returns `['12', '22', '32']`
+   *
+   * @param row A comma-separated row of values with possibly merged values
+   * @return std::deque<std::string> List of rows that can be merged back to
+   * `row`.
+   */
+  static std::deque<std::string> unmerge(std::string row);
+
+private:
+  /**
+   * @brief Recursive function that unwraps a merged row and saves the result to
+   * an array.
+   *
+   * @param columns Array where each element represents a column of a merged row
+   * @param i Index of last element in columns
+   * @param acc Accumulator for storing unmerged version of rows
+   */
+  static void unmerge(std::vector<std::string> columns, int i,
+                      std::deque<std::string> &acc);
+
+  /**
+   * @brief Splits a given string into a vector of substrings based on a
+   * specified delimiter.
+   *
+   * This function takes an input string and splits it into multiple substrings
+   * wherever the specified delimiter character appears. The resulting
+   * substrings are stored in a `std::vector<std::string>` and returned.
+   *
+   * Reference: https://stackoverflow.com/a/46931770/17627866
+   * 
+   * @param s The input string to be split.
+   * @param delim The character used as the delimiter to split the string.
+   * @return A vector containing the substrings after splitting the input
+   * string.
+   **/
+  static std::vector<std::string> split(const std::string &s, char delim);
 };
 
 #endif // VALIDATOR_H
