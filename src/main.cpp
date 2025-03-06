95fd8bb620acca9449d49ba2c0ddcc9f2ca47785
creme332 <65414576+creme332@users.noreply.github.com>
2025-03-06 14:25:33 +0400

    create general workflow for using algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,34 @@
+#include "CSVHandler.h"
+#include "algorithms/sample_algo.h"
 #include "validator.h"
 #include <iostream>
 
 int main() {
-  Validator validator;
-  auto result = validator.validate("hello", "world");
+  const std::string input_filename = "data.csv";
+  const std::string output_filename = "output.csv";
 
-  if (!result.first) {
-    std::cout << "Validation failed: " << result.second << std::endl;
+  // Validating CSV
+  auto csv_validation = CSVHandler::isValidCSV(input_filename);
+  if (csv_validation.first) {
+    std::cout << "CSV file is valid!" << std::endl;
   } else {
-    std::cout << "Validation passed!" << std::endl;
+    std::cerr << "Validation Error: " << csv_validation.second << std::endl;
+    return 0;
+  }
+
+  // Reading from CSV
+  std::string csvContent = CSVHandler::readCSV(input_filename);
+
+  // Compress CSV
+  SampleAlgorithm algo;
+  std::string output = algo.solve(csvContent);
+
+  auto validation = Validator::validate_output(csvContent, output);
+  std::cout << "Output validation: " << validation.second << std::endl;
+
+  // Writing to CSV
+  if (CSVHandler::writeToFile(output_filename, output)) {
+    std::cout << "Data written to " << output_filename << std::endl;
   }
 
   return 0;
